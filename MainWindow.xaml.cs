421a0eea0ef2cab3c38badcea139ba7bce88e390
Vaajin <dvn0001@edu.curio.nl>
2026-09-18 11:07:21 +0200

    Basisinkomst systeem

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -16,9 +17,34 @@ namespace idle_game
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double rosePetals = 0;
+        private double petalsPerSecond = 1;
+
+        private readonly DispatcherTimer gameTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer = new DispatcherTimer();
+            gameTimer.Interval = TimeSpan.FromMilliseconds(100);
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+
+            UpdateUI();
+        }
+
+        private void GameTimer_Tick(object? sender, EventArgs e)
+        {
+            rosePetals += petalsPerSecond / 10;
+
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            RosePetalsText.Text = $"🌹 {rosePetals:F1} Rose Petals";
+            PetalsPerSecondText.Text = $"+{petalsPerSecond:F1} Petals per second";
         }
     }
 }
