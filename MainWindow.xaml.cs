61cd70325f28af947fd3648c5d26c7d20ccd7664
Vaajin <dvn0001@edu.curio.nl>
2026-09-22 11:32:18 +0200

    Automatisering ontgrendelen gedaan
    Beekeeper automatisering

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,12 @@ namespace idle_game
         private double petalsPerSecond = 1;
         private double wateringCanCost = 10;
 
+        private double beeKeeperCost = 100;
+        private bool beeKeeperUnlocked = false;
+        private int beeKeeperProductions = 0;
+
         private readonly DispatcherTimer gameTimer;
+        private readonly DispatcherTimer beeKeeperTimer;
 
         public MainWindow()
         {
@@ -32,12 +37,27 @@ namespace idle_game
             gameTimer.Tick += GameTimer_Tick;
             gameTimer.Start();
 
+            beeKeeperTimer = new DispatcherTimer();
+            beeKeeperTimer.Interval = TimeSpan.FromSeconds(1);
+            beeKeeperTimer.Tick += BeeKeeperTimer_Tick;
+
             UpdateUI();
         }
 
         private void GameTimer_Tick(object? sender, EventArgs e)
         {
             rosePetals += petalsPerSecond / 10;
+
+            UpdateUI();
+        }
+
+        private void BeeKeeperTimer_Tick(object? sender, EventArgs e)
+        {
+            rosePetals += 1;
+
+            ProductionLogText.Text =
+                $"🐝 Bee Keeper: +1 Rose Petal ×{beeKeeperProductions}\n" +
+                $"Total produced: {beeKeeperProductions} Rose Petals";
 
             UpdateUI();
         }
@@ -60,12 +80,35 @@ namespace idle_game
             }
         }
 
+        private void BuyBeeKeeper_Click(object sender, RoutedEventArgs e)
+        {
+            if (rosePetals >= beeKeeperCost && !beeKeeperUnlocked)
+            {
+                rosePetals -= beeKeeperCost;
+                beeKeeperUnlocked = true;
+
+                beeKeeperTimer.Start();
+
+                UpdateUI();
+            }
+        }
+
         private void UpdateUI()
         {
             RosePetalsText.Text = $"🌹 {rosePetals:F1} Rose Petals";
             PetalsPerSecondText.Text = $"+{petalsPerSecond:F1} Petals per second";
             
             WateringCanButton.IsEnabled = rosePetals >= wateringCanCost;
+
+            if (beeKeeperUnlocked)
+            {
+                BeeKeeperButton.IsEnabled = false;
+                BeeKeeperButton.Content = "🐝 Bee Keeper Unlocked";
+            }
+            else
+            {
+                BeeKeeperButton.IsEnabled = rosePetals >= beeKeeperCost;
+            }
         }
     }
 }
